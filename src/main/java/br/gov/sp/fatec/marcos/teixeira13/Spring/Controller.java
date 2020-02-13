c0d3a6df5adf4c57cb033efa604bf281e386817c
marcos <marcos.teixeira@cittamobi.com.br>
2020-02-13 18:04:42 -0300

    Adicionado imagem à mensagem inicial

--- src/main/java/br/gov/sp/fatec/marcos/teixeira13/Spring/Controller.java
+++ src/main/java/br/gov/sp/fatec/marcos/teixeira13/Spring/Controller.java
@@ -8,6 +8,6 @@ public class Controller {
     @RequestMapping("/")
     @ResponseBody
     String index() {
-        return "<h1>Aprendendo String Boot</h1>";
+        return "<div style='text-align: center;'><img src='https://spring.io/img/homepage/icon-spring-boot.svg' width='250px'><br><h1>Aprendendo String Boot</h1></div>";
     }
 }
