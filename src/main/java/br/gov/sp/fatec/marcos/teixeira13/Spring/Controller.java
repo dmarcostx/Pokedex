6f3c3f8a633caed29ccbb6180b9afc64b33a6101
Marcos Teixeira <marcos.teixeira13@fatec.sp.gov.br>
2020-02-14 10:21:00 -0300

    Usando thymeleaf

--- src/main/java/br/gov/sp/fatec/marcos/teixeira13/Spring/Controller.java
+++ src/main/java/br/gov/sp/fatec/marcos/teixeira13/Spring/Controller.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
     @RequestMapping("/")
-    @ResponseBody
     String index() {
-        return "<div style='text-align: center;'><img src='https://spring.io/img/homepage/icon-spring-boot.svg' width='250px'><br><h1>Aprendendo String Boot</h1></div>";
+        return "index";
     }
 }
